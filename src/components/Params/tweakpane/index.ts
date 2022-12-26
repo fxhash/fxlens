ee15b10b045e42823901ec2e923e3ce1448933be
maerzhase <markus@lili16.de>
2022-12-26 12:25:48 +0100

    eslint

--- src/components/Params/tweakpane/index.ts
+++ src/components/Params/tweakpane/index.ts
@@ -4,7 +4,6 @@ import {FxParamDefinition, FxParamOptionsMap} from "types/fxparams"
 
 import { BladeController, InputBindingApi, SliderTextController, View } from "@tweakpane/core"
 import {FxStringInputController} from "./plugins/StringInputPlugin/controller"
-
 export type ParameterDefinitions = Record<string, FxParamDefinition<any>>
 
 enum EParameterType {
@@ -25,7 +24,12 @@ enum EParameterControllerView {
 interface IParameterController {
   view: EParameterControllerView
   parseValue: (v: string) => any
-  updateBinding?: ( binding: InputBindingApi<any, any>, definiton: FxParamDefinition<any>, p?: Pane, v?: ParameterValueMap) => void
+  updateBinding?: (
+    binding: InputBindingApi<any, any>,
+    definiton: FxParamDefinition<any>,
+    p?: Pane,
+    v?: ParameterValueMap
+  ) => void
 }
 
 interface IParameterControlDefinition {
@@ -50,10 +54,11 @@ export const parameterControlsDefinition: Record<
       view: EParameterControllerView.string,
       parseValue: (v: string) => String(v),
       updateBinding: (binding, definition) => {
-        const controller = (binding.controller_.valueController as FxStringInputController);
-        controller.props.set('minLength', definition.options?.minLength)
-        controller.props.set('maxLength', definition.options?.maxLength)
-      }
+        const controller = binding.controller_
+          .valueController as FxStringInputController
+        controller.props.set("minLength", definition.options?.minLength)
+        controller.props.set("maxLength", definition.options?.maxLength)
+      },
     },
   },
   [EParameterType.number]: {
@@ -62,12 +67,19 @@ export const parameterControlsDefinition: Record<
       view: EParameterControllerView.string,
       parseValue: (v: string) => Number(v),
       updateBinding: (binding, definition, p, v) => {
-        if (!p || !v) return;
-        const controller = (binding.controller_.valueController as SliderTextController)
+        if (!p || !v) return
+        const controller = binding.controller_
+          .valueController as SliderTextController
         // you cannot update the step of a number controller so we need to re-init the input
         // @ts-ignore
-        if(controller.sliderController.baseStep_ !== definition.options?.step) {
-          const index = p.children.findIndex((input) => (input as InputBindingApi<any,any>).controller_.binding.target.key === definition.id)
+        if (
+          controller.sliderController.baseStep_ !== definition.options?.step
+        ) {
+          const index = p.children.findIndex(
+            (input) =>
+              (input as InputBindingApi<any, any>).controller_.binding.target
+                .key === definition.id
+          )
           p.remove(binding)
           p.addInput(v!, definition.id, {
             view: EParameterControllerView.string,
@@ -76,18 +88,24 @@ export const parameterControlsDefinition: Record<
             ...definition.options,
           })
         } else {
-          const {min, max} = definition?.options
-          controller.sliderController.props.set('minValue', definition.options?.min)
-          controller.sliderController.props.set('maxValue', definition.options?.max)
+          const { min, max } = definition?.options
+          controller.sliderController.props.set(
+            "minValue",
+            definition.options?.min
+          )
+          controller.sliderController.props.set(
+            "maxValue",
+            definition.options?.max
+          )
           if (v[definition.id] > max) {
             v[definition.id] = max
             binding.refresh()
-          } else if(v[definition.id] < min) {
+          } else if (v[definition.id] < min) {
             v[definition.id] = min
-            binding.refresh();
+            binding.refresh()
           }
         }
-      }
+      },
     },
   },
   [EParameterType.boolean]: {
@@ -112,7 +130,7 @@ export function createFxPane(
   container: HTMLElement,
   params: ParameterDefinitions,
   p?: Pane,
-  v?: ParameterValueMap,
+  v?: ParameterValueMap
 ): [Pane, ParameterValueMap] {
   const pane = p || new Pane({ container })
   pane.registerPlugin(StringInputPlugin)
@@ -120,33 +138,43 @@ export function createFxPane(
     const paramDefinition = params[key]
     const { controller } =
       parameterControlsDefinition[paramDefinition.type as EParameterType]
-    const value = (v as ParameterValueMap)?.[key] ||controller.parseValue( params[key].default)
-    acc[key] = value;
+    const value =
+      (v as ParameterValueMap)?.[key] ||
+      controller.parseValue(params[key].default)
+    acc[key] = value
     return acc
-  }, v || {} as ParameterValueMap)
+  }, v || ({} as ParameterValueMap))
   Object.keys(params).forEach((key: string) => {
     const paramDefinition = params[key]
     if (!paramDefinition) return
     const { controller } =
       parameterControlsDefinition[paramDefinition.type as EParameterType]
-    const inputBinding = pane.children.find((input) => (input as InputBindingApi<any,any>).controller_.binding.target.key === key) as InputBindingApi<any, any>
-    if(!inputBinding) {
+    const inputBinding = pane.children.find(
+      (input) =>
+        (input as InputBindingApi<any, any>).controller_.binding.target.key ===
+        key
+    ) as InputBindingApi<any, any>
+    if (!inputBinding) {
       pane.addInput(valueMap, key, {
         view: controller.view,
         label: paramDefinition.name,
         ...paramDefinition.options,
       })
     } else {
-      console.log(inputBinding)
-      if (inputBinding.controller_.props.get("label") !== paramDefinition.name) {
+      if (
+        inputBinding.controller_.props.get("label") !== paramDefinition.name
+      ) {
         inputBinding.controller_.props.set("label", paramDefinition.name)
       }
       controller.updateBinding?.(inputBinding, paramDefinition, p, valueMap)
     }
   })
   pane.children.forEach((input) => {
-    const coldBinding = !Object.keys(params).includes((input as InputBindingApi<any, any>).controller_.binding.target.key)
+    const coldBinding = !Object.keys(params).includes(
+      (input as InputBindingApi<any, any>).controller_.binding.target.key
+    )
     if (coldBinding) pane.remove(input)
   })
   return [pane, valueMap]
 }
+
