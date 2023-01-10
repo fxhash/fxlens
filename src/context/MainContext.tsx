0340a9077ed26efc611d9f67305efc09a152bf04
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2023-01-10 16:22:59 +0100

    ✨ small overall polish

--- src/context/MainContext.tsx
+++ src/context/MainContext.tsx
@@ -4,7 +4,6 @@ import { useEffect } from "react"
 import { createContext } from "react"
 import { decodeUrl } from "utils/url"
 
-
 /**
  * The Main Context will wait for the project URL to be available to render
  * its children elements, otherwise it will render a loader.
@@ -14,29 +13,29 @@ import { decodeUrl } from "utils/url"
  * are injected into the DOM at the first render call.
  */
 
-interface IMainContext {
+export interface IMainContext {
   // the base project URL
-  baseUrl: string;
+  baseUrl: string
   // the url currently loaded in the iframe
-  url: string;
-  setUrl: (url: string) => void;
+  url: string
+  setUrl: (url: string) => void
   // params pulled from the <iframe> element
-  params: any;
-  setParams: (params: any) => void;
+  params: any
+  setParams: (params: any) => void
   // params pulled the controls
-  datParams: any;
-  setDatParams: (params: any) => void;
+  datParams: any
+  setDatParams: (params: any) => void
   // params update - used for auto refreshing
-  datParamsUpdate: any;
-  setDatParamsUpdate: (params: any) => void;
+  datParamsUpdate: any
+  setDatParamsUpdate: (params: any) => void
   // features from the <iframe> element
-  features: any;
-  setFeatures: (features: any) => void;
+  features: any
+  setFeatures: (features: any) => void
   // hash from the <iframe> element
-  hash: any;
-  setHash: (hash: string) => void;
-  iframe: HTMLIFrameElement | null;
-  setIframe: (iframe: HTMLIFrameElement) => void;
+  hash: any
+  setHash: (hash: string) => void
+  iframe: HTMLIFrameElement | null
+  setIframe: (iframe: HTMLIFrameElement) => void
 }
 
 const defaultMainContext: IMainContext = {
@@ -55,31 +54,27 @@ const defaultMainContext: IMainContext = {
   setHash: () => {},
   iframe: null,
   setIframe: () => {},
-};
+}
 
 export const MainContext = createContext(defaultMainContext)
 
-type Props = PropsWithChildren<{
-
-}>
-export function MainProvider({
-  children,
-}: Props) {
-  const [baseUrl, _] = useState(decodeUrl(
-    new URLSearchParams(window.location.search).get("target") || ""
-  ))
+type Props = PropsWithChildren<{}>
+export function MainProvider({ children }: Props) {
+  const [baseUrl, _] = useState(
+    decodeUrl(new URLSearchParams(window.location.search).get("target") || "")
+  )
   // initialize the URL from the query parameter target
   const [url, setUrl] = useState(baseUrl)
 
   // params, features, and hash, will be pulled out of the iframe
   const [params, setParams] = useState<any>(null)
   const [features, setFeatures] = useState<any>(null)
-  const [hash, setHash] = useState<any>(null);
-  
-  const [datParams, setDatParams] = useState<any>(null);
-  const [datParamsUpdate, setDatParamsUpdate] = useState<any>(null);
+  const [hash, setHash] = useState<any>(null)
 
-  const [iframe, setIframe] = useState<HTMLIFrameElement|null>(null)
+  const [datParams, setDatParams] = useState<any>(null)
+  const [datParamsUpdate, setDatParamsUpdate] = useState<any>(null)
+
+  const [iframe, setIframe] = useState<HTMLIFrameElement | null>(null)
 
   const context: IMainContext = {
     baseUrl,
@@ -97,11 +92,7 @@ export function MainProvider({
     setHash,
     iframe,
     setIframe,
-  };
+  }
 
-  return (
-    <MainContext.Provider value={context}>
-      {children}
-    </MainContext.Provider>
-  )
+  return <MainContext.Provider value={context}>{children}</MainContext.Provider>
 }
