e4a46519e73bc4965781c9616837d14f8d88d8c2
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2023-01-10 16:22:59 +0100

    Γò¼├┤Γö¼├║Γö¼ΓöÉ small overall polish

--- src/containers/Panel/PanelControls.tsx
+++ src/containers/Panel/PanelControls.tsx
@@ -1,41 +1,47 @@
 import style from "./PanelControls.module.scss"
 import cs from "classnames"
-import { useContext, useEffect, useState } from "react"
-import { MainContext } from "context/MainContext"
+import { useCallback, useContext, useEffect, useRef, useState } from "react"
+import { IMainContext, MainContext } from "context/MainContext"
 import { serializeParams } from "utils/fxparams"
-import {ParamsContext} from "context/Params"
+import { ParamsContext } from "context/Params"
+import { ParameterValueMap } from "components/Params/tweakpane"
+import { FxParamDefinition } from "types/fxparams"
+import debounce from "lodash.debounce"
 
-const updateIframe = (ctx: any, data: any, params: any) => {
-  const bytes = serializeParams(data, params);
-  const p = [`fxhash=${ctx.hash}`, `fxparams=0x${bytes}`];
-  const target = `${ctx.baseUrl}?${p.join("&")}`;
+type TUpdateIframe = (
+  ctx: IMainContext,
+  data?: ParameterValueMap,
+  params?: FxParamDefinition<any>[]
+) => void
+
+const updateIframe: TUpdateIframe = (ctx, data, params) => {
+  const bytes = serializeParams(data, params || [])
+  const p = [`fxhash=${ctx.hash}`, `fxparams=0x${bytes}`]
+  const target = `${ctx.baseUrl}?${p.join("&")}`
   if (ctx.iframe) {
-    ctx.iframe.contentWindow?.location.replace(target);
+    ctx.iframe.contentWindow?.location.replace(target)
   }
 }
 
-interface Props {
-  
-}
-export function PanelControls({
-  
-}: Props) {
+interface Props {}
+export function PanelControls({}: Props) {
   const ctx = useContext(MainContext)
   const params = useContext(ParamsContext)
 
-  const [autoUpdate, setAutoUpdate] = useState(false);
-  const onAutoUpdate = () => {
-    setAutoUpdate(!autoUpdate);
-  }
+  const [autoUpdate, setAutoUpdate] = useState(false)
+
+  const updateIframeDebounced = useCallback<TUpdateIframe>(
+    debounce<TUpdateIframe>((ctx, data, params) => {
+      updateIframe(ctx, data, params)
+    }, 200),
+    []
+  )
 
   useEffect(() => {
-      if (autoUpdate) {
-        updateIframe(ctx, params.data, params.params);
-      }
-    },
-    [ctx.hash, params.data]
-  );
-
+    if (autoUpdate) {
+      updateIframeDebounced(ctx, params.data!, params.params!)
+    }
+  }, [ctx.hash, JSON.stringify(params.data)])
 
   return (
     <div className={style.controlPanel}>
@@ -44,7 +50,7 @@ export function PanelControls({
         id="updateCheckbox"
         type="checkbox"
         checked={autoUpdate}
-        onClick={onAutoUpdate}
+        onClick={() => setAutoUpdate(!autoUpdate)}
       />
       <label htmlFor="updateCheckbox" className={style.checkbox}>
         auto-apply on settings update
@@ -52,25 +58,21 @@ export function PanelControls({
       <button
         type="button"
         onClick={() => {
-          if (!params.params) return;
-          const bytes = serializeParams(params.data, params.params);
-          const p = [`fxhash=${ctx.hash}`, `fxparams=0x${bytes}`];
-          const target = `${ctx.baseUrl}?${p.join("&")}`;
-          window.open(target);
+          if (!params.params) return
+          const bytes = serializeParams(params.data, params.params)
+          const p = [`fxhash=${ctx.hash}`, `fxparams=0x${bytes}`]
+          const target = `${ctx.baseUrl}?${p.join("&")}`
+          window.open(target)
         }}
       >
         Open project in new tab
       </button>
       <button
         type="button"
-        onClick={() => {
-          console.log(ctx)
-          console.log(params)
-          updateIframe(ctx, params.data, params.params);
-        }}
+        onClick={() => updateIframe(ctx, params.data, params.params)}
       >
         Refresh
       </button>
     </div>
-  );
+  )
 }
