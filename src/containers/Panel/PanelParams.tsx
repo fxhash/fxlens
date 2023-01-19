a275275fb1b8146fb85e198107c3e9f828dde0b1
maerzhase <markus@lili16.de>
2023-01-19 21:14:56 +0100

    refactor params context

--- src/containers/Panel/PanelParams.tsx
+++ src/containers/Panel/PanelParams.tsx
@@ -1,13 +1,14 @@
 import {Controls} from "components/FxParams/Controls";
 import { PanelGroup } from "components/Panel/PanelGroup";
 import { useContext } from "react"
-import { MainContext } from "context/MainContext"
+import {FxParamsContext} from "components/FxParams/Context";
+
 interface Props {}
 
 export function PanelParams({
   
 }: Props) {
-    const ctx = useContext(MainContext);
+    const ctx = useContext(FxParamsContext);
 
   return (
     <PanelGroup
