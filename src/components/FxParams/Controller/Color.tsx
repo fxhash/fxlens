3a16bdafa8d50e9798bf309fcef17343f85c16f0
maerzhase <markus@lili16.de>
2023-01-19 10:27:57 +0100

    implement custom color controller

--- src/components/FxParams/Controller/Color.tsx
+++ src/components/FxParams/Controller/Color.tsx
@@ -1,9 +1,37 @@
-import { FxParamControllerProps, HTMLInputController, HTMLInputControllerWithTextInput } from "./Controller"
+import { useState, useEffect, useRef, LegacyRef, MutableRefObject, RefObject, ChangeEvent } from "react";
+import { ChromePicker, ColorResult } from "react-color"
+import { FxParamControllerProps, Controller, BaseInput } from "./Controller"
 import classes from './Controller.module.scss'
 
 export function ColorController(props: FxParamControllerProps<"color">) {
-
+  const ref = useRef<HTMLDivElement>(null);
+  const { label, id, onChange, value, layout = "box" } = props;
+  const [showPicker, setShowPicker] = useState(false)
+  const handleToggleShowPicker = () => {
+    setShowPicker(show => !show)
+  }
+  const handlePickerChange = (color: ColorResult) => {
+    onChange(color.hex)
+  }
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value)
+  }
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+       if (ref.current && !ref.current?.contains(event.target as Node)) {
+         setShowPicker(false) 
+       }
+    }
+    window.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [ref])
   return (
-    <HTMLInputControllerWithTextInput type="color" layout="box" className={classes.colorInput}{...props} />
+    <Controller id={id} label={label} layout={layout} className={classes.pickerWrapper} inputContainerProps={{ref}}>
+      <button className={classes.square} style={{ background: value}} onClick={handleToggleShowPicker} />
+      <BaseInput type="text" id={`text-${id}`} onChange={handleInputChange} value={value} autoComplete="off" maxLength={9} minLength={2} />
+      {showPicker && <ChromePicker  color={value} onChange={handlePickerChange} className={classes.picker} />}
+    </Controller>
   )
 }
