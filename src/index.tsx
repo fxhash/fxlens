2c7e35d22a85166841d00793338b4cf278b3aa9d
maerzhase <markus@lili16.de>
2023-01-24 18:57:46 +0100

    communicate with artwork through postMessage api

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,6 @@ import "./index.scss"
 import { Root } from "containers/Root"
 import { App } from "containers/App"
 
-// allows this document to interact directly with the <iframe> even if not
-// under the same port
-document.domain = "localhost"
-
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
