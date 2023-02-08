9b0c5887827b6d33096d140df5170101730199c7
maerzhase <markus@lili16.de>
2023-02-08 16:19:11 +0100

    only give attention to bytesize when reached 50%

--- src/containers/Panel/PanelParams.tsx
+++ src/containers/Panel/PanelParams.tsx
@@ -4,15 +4,20 @@ import { useContext } from "react"
 import { FxParamsContext } from "components/FxParams/Context"
 import { ProgressBar } from "components/ProgressBar/ProgressBar"
 
+const MAX_BYTES = 50000
+
 export function PanelParams() {
   const ctx = useContext(FxParamsContext)
-
+  const bytes = ctx.byteSize || 0
+  const byteAttention = bytes >= MAX_BYTES / 2
   return (
     <PanelGroup
       title="Params"
-      description="fx(params) can be defines in your code and are pulled in real time from the code running. Parameter value space is limited to 50kb."
+      description={`fx(params) can be defines in your code and are pulled in real time from the code running. ${
+        byteAttention ? `Parameter value space is limited to 50kb.` : ""
+      }`}
     >
-      <ProgressBar max={50000} progress={ctx.byteSize || 0} />
+      {byteAttention && <ProgressBar max={MAX_BYTES} progress={bytes} />}
       <Controls params={ctx.params} />
     </PanelGroup>
   )
