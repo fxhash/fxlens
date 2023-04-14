7d0a50b64d22d7e7c4cdcde413cf718b166ef81d
Karsten Schmidt <k@thi.ng>
2023-04-14 17:39:19 +0200

    refactor: update fxparam (de)serializers
    Major simplifications of various fxparam related functions:
    - add U8/32/64 hex formatters
    - add asBytes() hexbyte string parser
    - add minmax() helper for enforcing optional ranges and dedupe logic
    - update/simplify rgbaToHex()()
    - update completeHexColor():
      - add 4-digit hex color support
      - fix: enforce 8-digit result (even if incomplete input)
    - update/simplify serializers/deserializers for all param types
    - fix string param serialization to safely handle non-ASCII chars
      (using UTF-8 representation)
    - add/update docstrings

--- src/components/FxParams/utils.ts
+++ src/components/FxParams/utils.ts
@@ -8,67 +8,88 @@ import {
   FxParamsData,
 } from "./types"
 
-export function rgbaToHex(r: number, g: number, b: number, a: number): string {
-  const outParts = [
-    r.toString(16),
-    g.toString(16),
-    b.toString(16),
-    Math.round(a * 255)
-      .toString(16)
-      .substring(0, 2),
-  ]
-
-  // Pad single-digit output values
-  outParts.forEach(function (part, i) {
-    if (part.length === 1) {
-      outParts[i] = "0" + part
-    }
-  })
-
-  return "#" + outParts.join("")
+export function rgbaToHex(r: number, g: number, b: number, a: number) {
+  return "#" + [r, g, b, (a * 255 + 0.5) | 0].map(U8).join("")
 }
 
 function completeHexColor(hexCode: hexString | string): string {
   let hex = hexCode.replace("#", "")
-  if (hex.length === 6) {
+  const [r, g, b, a] = hex
+  if (hex.length === 3) {
+    hex = `${r}${r}${g}${g}${b}${b}ff`
+  } else if (hex.length === 4) {
+    hex = `${r}${r}${g}${g}${b}${b}${a}${a}`
+  } else if (hex.length === 6) {
     hex = `${hex}ff`
   }
-  if (hex.length === 3) {
-    hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}ff`
-  }
-  return hex
+  return hex.padEnd(8, "0")
 }
 
 export function hexToRgba(hexCode: hexString) {
-  const hex = completeHexColor(hexCode)
-
-  const r = parseInt(hex.substring(0, 2), 16)
-  const g = parseInt(hex.substring(2, 4), 16)
-  const b = parseInt(hex.substring(4, 6), 16)
-  const a =
-    Math.round(
-      (parseInt(hex.substring(6, 8), 16) / 255 + Number.EPSILON) * 100
-    ) / 100
-
+  const [r, g, b, $a] = [...asBytes(completeHexColor(hexCode), 4)]
+  const a = Math.round(($a / 255 + Number.EPSILON) * 100) / 100
   return { r, g, b, a }
 }
 
-const stringToHex = function (s: string) {
-  let rtn = ""
-  for (let i = 0; i < s.length; i++) {
-    rtn += s.charCodeAt(i).toString(16).padStart(4, "0")
-  }
-  return rtn
+/**
+ * Formats `x` as unsigned 8bit hex string.
+ *
+ * @param x
+ */
+function U8(x: number) {
+  x >>>= 0
+  return x < 10 ? "0" + x : x.toString(16)
 }
-const hexToString = function (h: string) {
-  const hx = h.match(/.{1,4}/g) || []
-  let rtn = ""
-  for (let i = 0; i < hx.length; i++) {
-    const int = parseInt(hx[i], 16)
-    if (int === 0) break
-    rtn += String.fromCharCode(int)
+
+/**
+ * Formats `x` as unsigned 32bit hex string.
+ *
+ * @param x
+ */
+function U32(x: number) {
+  return (x >>> 0).toString(16).padStart(8, "0")
+}
+
+/**
+ * Formats `x` as unsigned 64bit hex string.
+ *
+ * @param x
+ */
+function U64(x: bigint) {
+  return BigInt.asUintN(64, x).toString(16).padStart(16, "0")
+}
+
+/**
+ * Takes a string of hex bytes and yields sequence of `n` parsed byte values.
+ *
+ * @param hex
+ * @param n
+ */
+function* asBytes(hex: string, n: number) {
+  n *= 2
+  if (hex.length < n) throw new Error("input too short")
+  for (let i = 0; i < n; i += 2) yield parseInt(hex.substring(i, i + 2), 16)
+}
+
+/**
+ * Takes optional min/max values and their defaults. Returns new min/max
+ * (clamped to interval defined by defaults).
+ *
+ * @param $min
+ * @param $max
+ * @param minDef
+ * @param maxDef
+ */
+function minmax(
+  $min?: number,
+  $max?: number,
+  minDef = Number.MIN_SAFE_INTEGER,
+  maxDef = Number.MAX_SAFE_INTEGER
+) {
+  return {
+    min: $min !== undefined ? Math.max(Number($min), minDef) : minDef,
+    max: $max !== undefined ? Math.min(Number($max), maxDef) : maxDef,
   }
-  return rtn
 }
 
 export const MIN_SAFE_INT64 = BigInt("-9223372036854775808")
@@ -76,132 +97,101 @@ export const MAX_SAFE_INT64 = BigInt("9223372036854775807")
 
 export const ParameterProcessors: FxParamProcessors = {
   number: {
-    serialize: (input) => {
-      const view = new DataView(new ArrayBuffer(8))
-      view.setFloat64(0, input as number)
-      return view.getBigUint64(0).toString(16).padStart(16, "0")
-    },
-    deserialize: (input) => {
-      const view = new DataView(new ArrayBuffer(8))
-      for (let i = 0; i < 8; i++) {
-        view.setUint8(i, parseInt(input.substring(i * 2, i * 2 + 2), 16))
-      }
-      return view.getFloat64(0)
-    },
+    serialize: (input) =>
+      U64(new BigUint64Array(new Float64Array([input]).buffer)[0]),
+
+    deserialize: (input) =>
+      new Float64Array(new BigUint64Array([BigInt("0x" + input)]).buffer)[0],
+
     bytesLength: () => 8,
-    random: (definition) => {
-      let min = Number.MIN_SAFE_INTEGER
-      if (typeof definition.options?.min !== "undefined")
-        min = Number(definition.options.min)
-      let max = Number.MAX_SAFE_INTEGER
-      if (typeof definition.options?.max !== "undefined")
-        max = Number(definition.options.max)
-      max = Math.min(max, Number.MAX_SAFE_INTEGER)
-      min = Math.max(min, Number.MIN_SAFE_INTEGER)
+
+    random: ({ options }) => {
+      const { min, max } = minmax(options?.min, options?.max)
       const v = Math.random() * (max - min) + min
-      if (definition?.options?.step) {
-        const t = 1.0 / definition?.options?.step
+      if (options?.step) {
+        const t = 1.0 / options.step
         return Math.round(v * t) / t
       }
       return v
     },
   },
+
   bigint: {
-    serialize: (input: any) => {
-      const view = new DataView(new ArrayBuffer(8))
-      view.setBigInt64(0, BigInt(input))
-      return view.getBigUint64(0).toString(16).padStart(16, "0")
-    },
-    deserialize: (input: any) => {
-      const view = new DataView(new ArrayBuffer(8))
-      for (let i = 0; i < 8; i++) {
-        view.setUint8(i, parseInt(input.substring(i * 2, i * 2 + 2), 16))
-      }
-      return view.getBigInt64(0)
-    },
+    serialize: U64,
+
+    deserialize: (input) => BigInt.asIntN(64, BigInt("0x" + input)),
+
     bytesLength: () => 8,
-    random: (definition) => {
+
+    random: ({ options }) => {
       let min = MIN_SAFE_INT64
       let max = MAX_SAFE_INT64
-      if (typeof definition.options?.min !== "undefined")
-        min = BigInt(definition.options.min)
-      if (typeof definition.options?.max !== "undefined")
-        max = BigInt(definition.options.max)
+      if (options?.min !== undefined) {
+        min = BigInt(options.min)
+        if (min < MIN_SAFE_INT64) min = MIN_SAFE_INT64
+      }
+      if (options?.max !== undefined) {
+        max = BigInt(options.max)
+        if (max > MAX_SAFE_INT64) max = MAX_SAFE_INT64
+      }
       const range = max - min
       const bits = range.toString(2).length
       let random
       do {
         random = BigInt(
-          "0b" +
+          "0x" +
             Array.from(
               crypto.getRandomValues(new Uint8Array(Math.ceil(bits / 8)))
             )
-              .map((b) => b.toString(2).padStart(8, "0"))
+              .map(U8)
               .join("")
         )
       } while (random > range)
       return random + min
     },
   },
+
   boolean: {
-    serialize: (input) => {
-      return typeof input === "boolean"
-        ? input
-          ? "01"
-          : "00"
-        : typeof input === "string"
-        ? input === "true"
-          ? "01"
-          : "00"
-        : "00"
-    },
-    deserialize: (input) => {
-      return input === "00" ? false : true
-    },
+    serialize: (input) => (!!input ? "01" : "00"),
+
+    deserialize: (input) => input !== "00",
+
     bytesLength: () => 1,
+
     random: () => Math.random() < 0.5,
   },
 
   color: {
-    serialize: (input: string) => {
-      return completeHexColor(input)
-    },
+    serialize: (input: string) => completeHexColor(input),
 
-    deserialize: (input): hexString => {
-      return input as hexString
-    },
+    deserialize: (input) => input,
+
     bytesLength: () => 4,
-    transform: (input) => {
-      return `#${completeHexColor(input)}`
-    },
-    random: () =>
-      `${[...Array(8)]
-        .map(() => Math.floor(Math.random() * 16).toString(16))
-        .join("")}`,
+
+    transform: (input) => `#${completeHexColor(input)}`,
+
+    random: () => U32(Math.random() * 0x100000000),
   },
 
   string: {
     serialize: (input) => {
-      let hex = stringToHex(input.substring(0, 64))
-      hex = hex.padEnd(64 * 4, "0")
-      return hex
+      const buf = new Uint8Array(64)
+      new TextEncoder().encodeInto(input, buf)
+      return Array.from(buf).map(U8).join("")
     },
 
     deserialize: (input) => {
-      return hexToString(input)
+      const buf = new Uint8Array(asBytes(input, 64))
+      const idx = buf.indexOf(0)
+      return new TextDecoder().decode(idx !== -1 ? buf.slice(0, idx) : buf)
     },
 
     bytesLength: () => 64 * 2,
-    random: (definition) => {
-      let min = 0
-      if (typeof definition.options?.minLength !== "undefined")
-        min = definition.options.minLength
-      let max = 64
-      if (typeof definition.options?.maxLength !== "undefined")
-        max = definition.options.maxLength
+    random: ({ options }) => {
+      const { min, max } = minmax(options?.minLength, options?.maxLength, 0, 64)
       const length = Math.round(Math.random() * (max - min) + min)
       return [...Array(length)]
-        .map((i) => (~~(Math.random() * 36)).toString(36))
+        .map(() => (~~(Math.random() * 36)).toString(36))
         .join("")
     },
   },
@@ -209,39 +199,43 @@ export const ParameterProcessors: FxParamProcessors = {
   select: {
     serialize: (input, def) => {
       // find the index of the input in the array of options
-      return Math.min(255, def.options?.options?.indexOf(input) || 0)
-        .toString(16)
-        .padStart(2, "0")
+      return U8(
+        Math.max(0, Math.min(255, def.options?.options?.indexOf(input) || 0))
+      )
     },
 
-    deserialize: (input, def) => {
+    deserialize: (input, { options }) => {
       // get the index, which is the input
       const idx = parseInt(input, 16)
-      return def.options?.options?.[idx] || def.options?.options?.[0] || ""
+      return options?.options?.[idx] || options?.options?.[0] || ""
     },
 
     bytesLength: () => 1, // index between 0 and 255
-    random: (definition) => {
-      const index = Math.round(
-        Math.random() * (definition.options.options.length - 1) + 0
-      )
-      return definition?.options?.options[index]
+    random: ({ options }) => {
+      const index = (Math.random() * options.options.length) | 0
+      return options?.options[index]
     },
   },
 }
 
-// params are injected into the piece using the binary representation of the
-// numbers, to keep precision
+/**
+ * Serializes given `params` object to hex byte string, using provided param
+ * `definitions`. Params are injected into the piece using the binary
+ * representation of the numbers, to keep precision
+ *
+ * @param params
+ * @param definitions
+ */
 export function serializeParams(
   params: any,
-  definition: FxParamDefinition<any>[]
+  definitions: FxParamDefinition<any>[]
 ) {
   // a single hex string will be used for all the params
   let bytes = ""
-  if (!definition) return bytes
+  if (!definitions) return bytes
   // loop through each parameter from the definition to find the associated
   // parameter as set on the UI
-  for (const def of definition) {
+  for (const def of definitions) {
     const { id, type } = def
     const processor = ParameterProcessors[
       type as FxParamType
@@ -250,9 +244,9 @@ export function serializeParams(
 
     const v = params[id] as FxParamTypeMap[]
     const val =
-      typeof v !== "undefined"
+      v !== undefined
         ? v
-        : typeof def.default !== "undefined"
+        : def.default !== undefined
         ? def.default
         : processor.random(def)
     const serialized = processor.serialize(val, def)
@@ -262,16 +256,22 @@ export function serializeParams(
   return bytes
 }
 
-// takes an array of bytes, in hexadecimal format, and a parametric space
-// definition and outputs an array of parameters, mapping the definition and
-// validating input based on the definition constraints
+/**
+ * Takes a string of hex bytes and array of parameter definitions. Outputs an
+ * array of deserialized parameters, mapping the raw bytes to their parameter
+ * definitions and validating input based on the definition constraints
+ *
+ * @param bytes
+ * @param definitions
+ * @param options
+ */
 export function deserializeParams(
   bytes: string,
-  definition: FxParamDefinition<FxParamType>[],
+  definitions: FxParamDefinition<FxParamType>[],
   options: { withTransform?: boolean }
 ) {
   const params: Record<string, FxParamType> = {}
-  for (const def of definition) {
+  for (const def of definitions) {
     const processor = ParameterProcessors[
       def.type as FxParamType
     ] as FxParamProcessor<FxParamType>
@@ -289,8 +289,13 @@ export function deserializeParams(
   return params
 }
 
-// Consolidates parameters from both a params object provided by the token
-// and the dat object of params, which is stored by the controls component.
+/**
+ * Consolidates parameters from both a params object provided by the token and
+ * the dat object of params, which is stored by the controls component.
+ *
+ * @param params
+ * @param data
+ */
 export function consolidateParams(params: any, data: any) {
   if (!params) return []
 
@@ -306,7 +311,7 @@ export function consolidateParams(params: any, data: any) {
         type as FxParamType
       ] as FxParamProcessor<any>
       let v
-      if (typeof def === "undefined") v = processor.random(definition)
+      if (def === undefined) v = processor.random(definition)
       else v = def
       rtn[p].value = processor.transform?.(v) || v
     }
@@ -349,8 +354,7 @@ export function sumBytesParams(
 }
 
 export function stringifyParamsData(data: FxParamsData) {
-  return JSON.stringify(data, (key, value) => {
-    if (typeof value === "bigint") return value.toString()
-    return value
-  })
+  return JSON.stringify(data, (_, value) =>
+    typeof value === "bigint" ? value.toString() : value
+  )
 }
